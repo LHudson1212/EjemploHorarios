6276d15a50a81f9f6ded36b44dbf8bdedeb1d45b
SENA <SENA@DESKTOP-6C2N5SV>
2025-10-30 14:06:54 -0500

    se incorporo la vista de wilber con la funcion de extraer el excel y una nueva tabla para esta misma funcion que esta en el controlador

--- EjemploHorarios/Data/EjemploHorariosContext.cs
+++ EjemploHorarios/Data/EjemploHorariosContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EjemploHorarios.Models;
+using EjemploHorarios.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -14,11 +16,33 @@ namespace EjemploHorarios.Data
         // automatically whenever you change your model schema, please use data migrations.
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
+
         public EjemploHorariosContext() : base("name=EjemploHorariosContext")
         {
         }
+      
 
+        public virtual DbSet<Instructor> Instructor { get; set; }
+        public virtual DbSet<Ficha> Ficha { get; set; }
+        public virtual DbSet<Diseño_Curricular> Diseño_Curricular { get; set; }
+        public virtual DbSet<Programa_Formacion> Programa_Formacion { get; set; }
+        public virtual DbSet<Horario> Horario { get; set; }
+        public virtual DbSet<Asignacion_horario> Asignacion_horario { get; set; }
 
     }
+
 }
+        
+     
+
+
+ 
+          
+
+     
+
+
+
+
+
+
